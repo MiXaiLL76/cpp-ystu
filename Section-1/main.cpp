b3c3531c5c02557984818df1a3df7435eded6319
MiXaiLL76 <mike.milos@yandex.ru>
2018-02-03 22:34:07 +0300

    s-1

--- Section-1/main.cpp
+++ Section-1/main.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main() 
 { 
-    cout << "Hello, world!" << endl;
+    //Константные значения из задания
+    const double a = 2.5f;
+    const double b = 0.5f;    
+    const double x_arr[] = {8,1};
+    
+    int x_len = sizeof(x_arr) / sizeof(x_arr[0]); // Длинна массива
+
+    cout << "Кол-во переменных X: " << x_len << endl;
+    cout << "a: " << a << endl;
+    cout << "b: " << b << endl << endl;//Вывод с доп. пропуском строки
+
+    double x = 0;//Перменная для перебора
+    for(int i=0;i<x_len;i++){
+        x = x_arr[i];//Задаём значение
+        double Y = pow((x+a),5) * (atan(x) - sqrt(abs(pow(x-a,2))) + pow(log(pow(x,2)+1),2)); // формула из задания
+        double F = sin(x) - exp(-a*x) + log(abs(x-a) + 2);
+
+        cout << "x: " << x << endl;
+        cout << "Y: " << Y << endl;
+        cout << "F: " << F << endl << endl;
+    }
+   
     return 0; 
 }
