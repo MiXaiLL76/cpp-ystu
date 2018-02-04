ef958b86e29ddd300d5a5587eaaedf4001d522fd
MiXaiLL76 <mike.milos@yandex.ru>
2018-02-04 17:42:36 +0300

    s4

--- Section-4/main.cpp
+++ Section-4/main.cpp
@@ -6,22 +6,22 @@ int main()
 { 
     srand(static_cast<unsigned int>(time(NULL)));//Делаем переобновление случайных значений
 
-    double  x = rand() % 40 - 20,
+    double  x = rand() % 40 - 20,//координаты точки А
             a = rand() % 40 - 20,
             b = rand() % 40 - 20,
             c = rand() % 40 - 20,
             d = rand() % 40 - 20;
     
     //Вывод их пользователю
-    cout << "Точка Х: " << x << endl;
+    cout << "Точка А: " << x << endl;
     cout << "Отрезок [a, b]: [" << a << ", " << b << "]" << endl;
     cout << "Отрезок [c, d]: [" << c << ", " << d << "]" << endl;
 
     string ret_text = "";
-    if((x >= a && x <= b) || (x >= b && x <= a))ret_text+="Точка X принадлежит отрезку [a, b]\n";
-    if((x >= c && x <= d) || (x >= d && x <= c))ret_text+="Точка X принадлежит отрезку [c, d]\n";
+    if((x >= a && x <= b) || (x >= b && x <= a))ret_text+="Точка А принадлежит отрезку [a, b]\n";
+    if((x >= c && x <= d) || (x >= d && x <= c))ret_text+="Точка А принадлежит отрезку [c, d]\n";
     
-    if( ret_text.length() == 0)ret_text = "Точка Х не принадлежит этим отрезкам.";
+    if( ret_text.length() == 0)ret_text = "Точка А не принадлежит этим отрезкам.";
     cout << ret_text;
     return 0; 
 }
