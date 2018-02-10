351c2af28cd65fc8d2d7a8703ef4f41e992cf594
MiXaiLL76 <mike.milos@yandex.ru>
2018-02-10 14:20:41 +0300

    l

--- Section-7/main.cpp
+++ Section-7/main.cpp
@@ -4,13 +4,44 @@
 using namespace std;
 
 double rand(double b, double e);//Представление!?
+int factorial(int n);
+
 int main() 
 { 
     srand(static_cast<unsigned int>(time(NULL)));//Делаем переобновление случайных значений
 
-    // "вводим" переменные      
+    // "вводим" переменные  
 
-    
+
+    const double  e = 0.00001f;
+    double  x = rand(-10,10),
+            xk = rand(x,20),
+            h  = rand(0,1);
+
+    //Вывод их пользователю
+    cout << "e: " << e << endl;
+    cout << "x0: " << x << endl;
+    cout << "xk: " << xk << endl;
+    cout << "h:  " << h << endl << endl;
+
+    do{
+        double  s = 0; // Бесконечная сумма S(x)  
+        double  a = 0; // Переменная 1 члена.
+
+        int n = 0;
+        
+        do{
+            a = pow(x,2*n+1)/factorial(2*n+1);
+            s += a; 
+            n++;
+        }while(abs(a) < e);
+
+
+        double y = sinh(x);
+        cout << "x: " << x << " | S(x): " << s << " | y(x): " << y << endl;
+
+        x += h;
+    }while(x < xk);
     return 0; 
 }
 
@@ -18,4 +49,9 @@ int main()
 double rand(double b, double e)//Функция самописная, честно стыреная с cyberforum
 {
    return b + (rand() % static_cast<unsigned int>((e - b) * 1000)) / 1000.f;   
+}
+
+int factorial(int n)
+{
+  return (n == 1 || n == 0) ? 1 : factorial(n - 1) * n;
 }
